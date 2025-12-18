eff6f24ef5d7c3e25b16e9a5de485bd536e575b8
Diego Palacios <diego.palacios@alumni.mondragon.edu>
2025-12-18 17:38:27 +0100

    Funcionalidades carga de datos

--- packages/Preprocesamiento/funciones_limpieza.py
+++ packages/Preprocesamiento/funciones_limpieza.py
@@ -1,14 +1,47 @@
+import os
+import pickle
 import pandas as pd
 
-def read_data(filename):
-    return pd.read_csv(filename, index_col = [0])
 
-def remove_column(df, colnames):
-    return df.drop(columns=colnames)
+def leer_datos(nombre_archivo, origen):
+    if origen.lower() in ['org', 'originales']:
+        ruta_datos = os.path.join('..', 'Datos', 'Originales')
+    elif origen.lower() in ['trf', 'transformados']:
+        ruta_datos = os.path.join('..', 'Datos', 'Transformados')
+    else:
+        print('El parámetro "origen" no está bien especificado')
+        return None
+    
+    ruta_archivo = os.path.join(ruta_datos, nombre_archivo)
+    return pd.read_csv(ruta_archivo, index_col = [0])
 
-def remove_rows_with_nas(df, colnames):
-    return df[df[colnames].notna()]
 
-def save_clean_data(df, file):
-    df.to_csv(file)
-    return None
+def guardar_objeto(obj, nombre, destino):
+    if destino.lower() in ['org', 'originales']:
+        destino = 'Originales'
+    elif destino.lower() in ['trf', 'transformados']:
+        destino = 'Transformados'
+    else:
+        print('El parámetro "destino" no está bien especificado')
+        return None
+    
+    nombre_extension = f'{nombre}.pkl'
+    with open(os.path.join('..', 'Datos', destino, nombre_extension), 'wb') as f:
+        pickle.dump(obj, f)
+    
+    return
+
+def cargar_objeto(nombre, origen):
+    if origen.lower() in ['org', 'originales']:
+        origen = 'Originales'
+    elif origen.lower() in ['trf', 'transformados']:
+        origen = 'Transformados'
+    else:
+        print('El parámetro "origen" no está bien especificado')
+        return None
+    
+    nombre_extension = f'{nombre}.pkl'
+    with open(os.path.join('..', 'Datos', origen, nombre_extension), 'rb') as f:
+        objeto_cargado = pickle.load(f)
+    
+    return objeto_cargado
