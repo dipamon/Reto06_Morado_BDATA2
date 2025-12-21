2151e3bc2c61c27c1c53dc6d8d51f0bdedf66964
Ibai Alario <ibai.alario@alumni.mondragon.edu>
2025-12-21 19:28:32 +0100

    Analisis exploratorio completado

--- packages/Preprocesamiento/funciones_limpieza.py
+++ packages/Preprocesamiento/funciones_limpieza.py
@@ -1,7 +1,18 @@
+import os
 import pandas as pd
 
-def read_data(filename):
-    return pd.read_csv(filename, index_col = [0])
+
+def leer_datos(nombre_archivo, origen):
+    if origen.lower() in ['org', 'originales']:
+        ruta_datos = os.path.join('..\\Datos', 'Originales')
+    elif origen.lower() in ['trf', 'transformados']:
+        ruta_datos = os.path.join('..\\Datos', 'Transformados')
+    else:
+        print('El parámetro "origen" no está bien especificado')
+        return None
+    
+    ruta_archivo = os.path.join(ruta_datos, nombre_archivo)
+    return pd.read_csv(ruta_archivo, index_col = [0])
 
 def remove_column(df, colnames):
     return df.drop(columns=colnames)
